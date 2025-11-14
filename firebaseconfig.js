6e4c0e7e8bae5b58c51f053e606de23d83efbbb6
cephiii94 <cecephard12@gmail.com>
2025-11-14 13:11:50 +0800

    add

---
+++ firebaseconfig.js
@@ -0,0 +1,10 @@
+// Contoh konfigurasi dari Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDlOea99ikDbHSEQ8XswmjyXMCk0P8lFtY",
+  authDomain: "project-notonlen.firebaseapp.com",
+  projectId: "project-notonlen",
+  storageBucket: "project-notonlen.firebasestorage.app",
+  messagingSenderId: "592072676592",
+  appId: "1:592072676592:web:e544183f12c61a9d261e35",
+  measurementId: "G-VL7HDPL94M"
+};
